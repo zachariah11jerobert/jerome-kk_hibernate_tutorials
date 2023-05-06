355662c699cd7dcf94e5b37d15102119cb0cd61e
jerome <zachariah11jerobert@gmail.com>
2023-05-06 17:33:32 +0530

    done read data for inheritance strategies

---
+++ Ex_16_InheritanceStrategy_3_tablePerClass_FetchData/src/main/java/com/infotech/entities/Person.java
@@ -0,0 +1,51 @@
+package com.infotech.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="person_table")
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
+public class Person {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name",length=100)
+	private String name;
+	
+	@Column(name="gender",length=10)
+	private String gender;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", gender=" + gender + "]";
+	}
+	
+}
